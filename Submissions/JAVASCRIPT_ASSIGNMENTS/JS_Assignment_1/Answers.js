d6877eb2a78bd9d5509e1fb7ac77d734fe1640bc
Aditya Sharma <adityasharam555@gmail.com>
2024-08-24 06:32:59 +0530

    Added js assignments in Submissions folder

---
+++ Submissions/JAVASCRIPT_ASSIGNMENTS/JS_Assignment_1/Answers.js
@@ -0,0 +1,73 @@
+
+
+// Answer 1:
+
+// const sayHello = () => {
+//     return "Hello World"
+// }
+
+// function sayHello() {
+//     return "Hello World";
+// }
+
+
+// Answer 2:
+
+// const doubleNumber = (num) => {
+//     return num * 2;
+// }
+
+// function doubleNumber(num) {
+//     return num * 2;
+// }
+
+// Answer 3:
+
+// const addTwoNumbers = (a, b) => {
+//     return a+b;
+// }
+
+// function addTwoNumbers(a, b) {
+//     return a+b;
+// }
+
+
+
+// Answer 4:
+
+// const doubleNumber = (num) => {
+//     return num * 2;
+// }
+
+
+// Answer 5:
+
+function subtractTen (num) {
+    return num-10;
+}
+
+// Answer 6:
+function isGreaterThanFive(num) {
+    return num > 5;
+}
+
+// Answer 7:
+function isBetweenOneAndTen(num) {
+    return (num>0) || (num<11)
+}
+
+// Answer 8:
+function isEven(num) {
+    return num%2 == 0 ?  "Even" : "Odd"
+}
+
+// Answer 9:
+function checkAge(age) {
+    return age >= 18 ? "Adult" : "Minor"
+}
+
+// Answer 10:
+function getDayType(day) {
+    return day > 5 ? "Weekend" : "Weekday"
+}
+
